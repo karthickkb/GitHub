063cb9eab8b812e882b0f81e2e5b993de788714e
karthickkb <kbkarthickbalaji@gmail.com>
2022-01-30 17:13:32 +0530

    password manager

--- src/org/tc1/Login.java
+++ src/org/tc1/Login.java
@@ -1,5 +1,12 @@
 package org.tc1;
 
-public class Login {
-
+public class Login 
+{
+public static void passWord() {
+	System.out.println("password");
+}
+public static void main(String args[])
+{
+	passWord();
+}
 }
